496aecec54f6de691594ba6d5775e45ae6e1081a
i054267 <yuan.wang01@sap.com>
2019-12-14 00:58:23 +0100

    first function finished

--- fashion.js
+++ fashion.js
@@ -1,24 +1,54 @@
 "use strict";
 
-var Crawler = require("crawler");
+const Crawler = require("crawler");
+const email = require("./email");
+const fs = require('fs');
+
+const subject = "bolide"; //"picotin";  //"bolide";
+const noResultText = "Hoppla!";
+const timeout = 1800; //second
+var lastTimestamp;
+
+fs.readFile('last_timestamp.txt', (err, data) => { 
+    if (err) throw err; 
+    console.log(data.toString()); 
+    lastTimestamp = parseInt(data.toString());
+}) 
 
 var c = new Crawler({
-    maxConnections : 10,
-    // This will be called for each crawled page
+    maxConnections : 1,
+    rateLimit: 30000, //millisecond
     callback : function (error, res, done) {
         if(error){
             console.log(error);
         }else{
             var $ = res.$;
-            var bags = [];
-            $('div.product-item').each(function (i, e) {
-                bags[i] = $(this).text();
-            });
-            console.log(bags);
+            if ($('div.main-title').text() == noResultText) {
+                email.sendMail(subject, noResultText);
+                console.log(noResultText);
+            } else {
+                var bags = [];
+                $('div.product-item').each(function (i, e) {
+                    bags[i] = $(this).text();
+                });
+                //console.log(bags);
+                //console.log(bags.length);
+                var currentTimestamp = Math.floor(Date.now() / 1000);
+                console.log("current", currentTimestamp);
+                console.log("last   ", lastTimestamp);
+                if ( currentTimestamp - lastTimestamp > timeout ) {
+                    email.sendMail(subject, bags.toString());
+                    lastTimestamp = currentTimestamp;
+                    fs.writeFile('last_timestamp.txt', lastTimestamp, (err) => { 
+                        if (err) throw err; 
+                    }) 
+                    console.log("send mail " + subject);
+                }
+            }
         }
         done();
     }
 });
 
-//c.queue('https://www.hermes.com/de/de/damen/taschen-und-kleinlederwaren/taschen-und-kleine-taschen/#fh_view_size=108&country=de&fh_location=--%2Fcategories%3C%7Bwomenbagsbagsclutches%7D&fh_start_index=72&positionsku=H060991CC18|relevance|Linie');
-c.queue('https://www.hermes.com/de/de/damen/taschen-und-kleinlederwaren/taschen-und-kleine-taschen/#fh_view_size=36&country=de&fh_location=--%2Fcategories%3C%7Bwomenbagsbagsclutches%7D&fh_start_index=300|relevance|Linie');
+const url = 'https://www.hermes.com/de/de/search/?s=' + subject;
+c.queue(url);
